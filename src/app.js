858f382e7d4be3febfd26c57503a11d8a12e5d21
Breno Silva <brenosilva1@gmail.com>
2018-04-13 22:24:47 -0300

    add models

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Database connection
+mongoose.connect('mongodb://brenopms:balta@ds135399.mlab.com:35399/balta')
 
 //Carrega as rotas
 const indexRoute = require('./routes/indexRoute');
