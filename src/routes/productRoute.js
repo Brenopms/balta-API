339e6c8ea29e0b56b1b40d6306e2cd3c62d3944f
Breno <brenosilva1@GMAIL.COM>
2018-04-23 22:18:33 -0300

    find by slug

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -4,9 +4,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
-router.get('/', controller.get)
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
 router.post('/', controller.post);
-router.put('/:id', controller.put)
-router.delete('/', controller.delete)
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
