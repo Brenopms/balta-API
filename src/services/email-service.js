9cade36efc87bd8d7427d8f4fd6542c3dad8d1e1
Breno <brenosilva1@GMAIL.COM>
2018-05-22 17:54:53 -0300

    add sendgrid email

---
+++ src/services/email-service.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const config = require('../config');
+const sendGrid = require('@sendgrid/mail');
+sendGrid.setApiKey(config.sendgridKey);
+exports.send = async (to, subject, body) => {
+    sendGrid.send({
+        to: to,
+        from: 'brenosilva1@gmail.com',
+        subject: subject,
+        html: body
+    });
+}
+
+
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com',
+//   from: 'test@example.com',
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail.send(msg);
