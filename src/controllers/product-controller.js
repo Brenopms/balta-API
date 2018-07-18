2024386f95ffeff6a684478008cf6674bf8573ac
Breno <brenosilva1@GMAIL.COM>
2018-07-17 23:54:29 -0300

    upload of images

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
 const repository  = require('../repositories/product-repository');
+const azure = require('azure-storage');
+const guid = require('guid');
+const config = require('../config');
 
 exports.get = async(req, res, next) => {
     try {
@@ -56,7 +59,33 @@ exports.post = async(req, res, next) => {
     }
 
     try {
-        await repository.create(req.body);
+         const blobSvc = azure.createBlobService(config.containerConnectionString);
+         let filename = `${guid.raw().toString()}.jpg`;
+         let rawData = req.body.image;
+         let matches = rawData.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+         let type = matches[1];
+         let buffer = new Buffer(matches[2], 'base64');
+
+         await blobSvc.createAppendBlobFromText('product-images', filename, buffer, {
+             contentType: type
+         }, (error, result, response) => {
+             if(error) {
+                 filename = 'default-product.png'
+             }
+         });
+
+         const {title, slug, description, price, tags} = req.body;
+         
+        await repository.create({
+            title: title,
+            slug: slug,
+            description: description,
+            price: price,
+            active: true,
+            tags: tags,
+            image: `https://baltateste.blob.core.windows.net/product-images/${filename}`
+        });
+
         res.status(201).send({
             message: "Product saved successfully"
         });
